e3bd190a987cdd2a519f7c3f9af281421c84397b
Eddy Kilonzo <eddymax3715@gmail.com>
2023-12-26 18:26:56 +0300

    added router

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 const express = require('express');
 const app = express(); 
+const port = 3000;
 
+//static files
+app.use(express.static('public'));
+app.use('/css', express.static( __dirname + 'public/css'));
+app.use('/images', express.static(__dirname + +'public/images'));
+app.use('/js', express.static(__dirname + 'public/js'));
+
+
+//templating engine
+
+app.set('views', './views');
 app.set('view engine', 'ejs');
+
+
+
+
 
 app.get('/', (req, res) => {
     
@@ -11,7 +26,7 @@ app.get('/', (req, res) => {
 
 
 
-const port = 3000;
+
 app.listen(port, (err) => {
     if (err) {
       console.error(`Error starting the server: ${err}`);
